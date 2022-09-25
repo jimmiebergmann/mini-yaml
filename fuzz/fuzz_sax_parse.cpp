8e6360e6340968603212a1b2b8611caef4314322
Jimmie Bergmann <jimmiebergmann@gmail.com>
2022-09-25 03:13:01 +0200

    Add libfuzzer support.

---
+++ fuzz/fuzz_sax_parse.cpp
@@ -0,0 +1,18 @@
+#include "yaml/yaml2.hpp"
+
+struct test_sax_handler {
+    void null() {}
+    void start_object() {}
+    void end_object() {}
+    void start_array() {}
+    void end_array() {}
+    void key(yaml::basic_string_view<uint8_t>) {}
+    void string(yaml::basic_string_view<uint8_t>) {}
+    void comment(yaml::basic_string_view<uint8_t>) {}
+};
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    test_sax_handler handler = {};
+    yaml::sax::parse(data, size, handler);
+    return 0;
+}
